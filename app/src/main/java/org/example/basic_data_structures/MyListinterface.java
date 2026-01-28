6a2bf0eef265f7a6ea2578b188687b42314484e3
Kriyansh Shah <k.s.shah1504@gmail.com>
2026-01-28 17:17:31 +0000

    List Started

---
+++ app/src/main/java/org/example/basic_data_structures/MyListinterface.java
@@ -0,0 +1,10 @@
+public package org.example.basic_data_structures;
+
+public interface MyListinterface {
+    void add(int index, E element);
+    boolean add(E element);
+    E get(int index);
+
+
+    int size();
+}
